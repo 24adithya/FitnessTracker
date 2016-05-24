7c04d607901f88e43666028051a429995d784f78
24adithya <24adithya@gmail.com>
2016-05-24 21:45:24 +0530

    added jquery and activity

--- src/main/java/org/pluralsight/controller/MinutesController.java
+++ src/main/java/org/pluralsight/controller/MinutesController.java
@@ -1,9 +1,15 @@
 package org.pluralsight.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.pluralsight.model.Activity;
 import org.pluralsight.model.Exercise;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MinutesController {
@@ -12,7 +18,28 @@ public class MinutesController {
 	public String addMinutes(@ModelAttribute ("exercise") Exercise exercise)
 	{
 		System.out.println("Minutes exercised : " + exercise.getMinutes());
+		System.out.println("Activity : " + exercise.getActivity());
 		return "addMinutes";
+	}
+	
+	@RequestMapping(value = "/activities", method = RequestMethod.GET)
+	public @ResponseBody List<Activity> findAllActivities()
+	{
+		List<Activity> activities = new ArrayList<Activity>();
+		
+		Activity run = new Activity();
+		run.setDesc("Run");
+		activities.add(run);
+		
+		Activity bike = new Activity();
+		bike.setDesc("Bike");
+		activities.add(bike);
+		
+		Activity swim = new Activity();
+		swim.setDesc("Swim");
+		activities.add(swim);
+		
+		return activities;
 	}
 	
 	/**
--- src/main/webapp/WEB-INF/jsp/addMinutes.jsp
+++ src/main/webapp/WEB-INF/jsp/addMinutes.jsp
@@ -7,6 +7,28 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Minutes of Spring training</title>
+
+<script type="text/javascript" src="jquery-2.2.4.js"></script>
+
+<script type="text/javascript">
+	$(document).ready(
+			function() {
+				$.getJSON('http://10.253.80.99:9080/FitnessTracker/activities.json'), {
+					ajax : 'true'
+				}, function(data) {
+					console.log(data);
+					var html = '<option value="">--Please select one--</option>';
+					var len = data.length;
+					for(var i = 0 ; i < len ; i++) {
+						html += '<option value="' + data[i].desc + '">' + data[i].desc + '</option>';
+						html += '</option>';
+						$('#activities').html(html);
+					}
+				}
+			});
+
+</script>
+
 </head>
 <body>	
 
@@ -16,9 +38,10 @@ Language <a href="?language=en">English</a> | <a href="?language=es">Spanish</a>
 		<tr>
 			<td><spring:message code="goal.title"/> </td>
 			<td><form:input path="minutes"/></td>
+			<td><form:select id="activities" path="activity"/></td>
 		</tr>
 		<tr>
-			<td colspan="2">
+			<td colspan="3">
 				<input type="submit" value="Enter exercise">
 			</td>
 		</tr>
